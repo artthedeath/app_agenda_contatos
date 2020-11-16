03c75298a4e60abb202e5399d4ace7df8531256c
Arthur Dias <arthuriusrodrigues@hotmail.com>
2020-11-15 23:58:05 -0300

    first commit

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,62 @@
+import 'package:app_agenda_contatos/helpers/contact_helpers.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  ContactHelper helper = ContactHelper();
+  List<Contact> contacts = List();
+
+  @override
+  void initState(){
+    super.initState();
+
+    helper.getAllContacts().then((list){
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount:contacts.length ,
+        itemBuilder: (context, index){
+
+        },
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index){
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child:Row(
+            children: [
+              
+            ],) ,
+          ),
+        ),
+    );
+  }
+}
